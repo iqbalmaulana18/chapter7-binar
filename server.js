843a332df16d2ff96bd224fa524f5ee5aea26e1b
Iqballm18 <iblmlna18@gmail.com>
2023-06-13 12:18:05 +0700

    setup passport

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
+const session = require('express-session');
 
 const dashboardRouter = require('./domains/dashboard/routes');
 const gameRouter = require('./domains/games/routes');
 const playerRouter = require('./domains/players/routes');
+const passport = require('./utils/passport');
 const roomRouter = require('./domains/rooms/routes')
 const userRouter = require('./domains/users/routes');
 
@@ -18,6 +20,15 @@ app.set('views', 'views');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use('/static', express.static('public/static'));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge: 60000,
+    },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(dashboardRouter);
 app.use(gameRouter);
