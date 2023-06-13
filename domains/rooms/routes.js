5a2aba6b7b14ed44bd06ddf3bff682f8bd870494
Iqballm18 <iblmlna18@gmail.com>
2023-06-13 13:04:59 +0700

    change domains folder to mvc implementation

--- domains/rooms/routes.js
+++ domains/rooms/routes.js
@@ -1,25 +1,11 @@
 const express = require('express');
 const { Room } = require('../../database/models');
 const authentication = require('../../middlewares/authentication');
+const { createRoom } = require('../players/controller');
 
 const roomRouter = express.Router();
 
-// endpoint for create new room
-roomRouter.post('/api/v1/rooms', authentication, async function(req, res) {
-    const ROCK_PAPER_SCISSOR = 1;
-
-    const room = await Room.create({
-       game_id: ROCK_PAPER_SCISSOR,
-       created_by: req.user.id,
-    });
-
-    res.json({
-        message: 'success create new room',
-        result: {
-            room_id: room.id,
-        },
-        error: null,
-    });
-});
+// endpoint untuk create room
+roomRouter.post('/api/v1/rooms', authentication, createRoom);
 
 module.exports = roomRouter;
